8a3a5c2980e1261670cc4bc70030a1c8bc0a82d9
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-03-09 09:08:27 +0200

    Created ListingsRepository

--- Data/Repositories/Listings/IListingsRepository.cs
+++ Data/Repositories/Listings/IListingsRepository.cs
@@ -2,11 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Auctions.Models;
 
 namespace Auctions.Data.Repositories.Listings
 {
-    public interface IListingsRepository
+    public interface IListingsRepository: IRepository<Listing>
     {
-        
+        new Task<Listing?> FindById(int id);
     }
 }
--- Data/Repositories/Listings/ListingsRepository.cs
+++ Data/Repositories/Listings/ListingsRepository.cs
@@ -2,11 +2,33 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Auctions.Models;
+using Microsoft.EntityFrameworkCore;
+using Rpg.Data.Repositories;
 
 namespace Auctions.Data.Repositories.Listings
 {
-    public class ListingsRepository
+    public class ListingsRepository : Repository<Listing>, IListingsRepository, IDisposable
     {
-        
+        private readonly DataContext context;
+
+        public ListingsRepository(DataContext context):base(context)
+        {
+            this.context=context;
+        }
+
+        new public async Task<Listing?> FindById(int id){
+            var listing = await context.Listings
+                .Include(l => l.User)
+                .Include(l => l.Comments)
+                .Include(l => l.Bids)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            return listing;
+        } 
+        public async void Dispose()
+        {
+            await context.DisposeAsync();
+            GC.SuppressFinalize(this);
+        }
     }
 }
