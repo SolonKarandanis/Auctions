dd4a551056bb08e0c967f37098ab8d06b398a77c
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-03-09 09:57:40 +0200

    Created ListingsController

---
+++ Controllers/ListingsController.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Auctions.Services.Bids;
+using Auctions.Services.Comments;
+using Auctions.Services.Listings;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Auctions.Controllers
+{
+    [Route("[controller]")]
+    public class ListingsController : Controller
+    {
+        private readonly ILogger<ListingsController> logger;
+        private readonly IListingsService listingsService;
+        private readonly IBidsService bidsService;
+        private readonly ICommentsService commentsService;
+        private readonly IWebHostEnvironment webHostEnvironment;
+
+        public ListingsController(
+            IListingsService listingsService, 
+            IWebHostEnvironment webHostEnvironment, 
+            IBidsService bidsService, 
+            ICommentsService commentsService,
+            ILogger<ListingsController> logger)
+        {
+            this.logger = logger;
+            this.listingsService = listingsService;
+            this.webHostEnvironment = webHostEnvironment;  
+            this.bidsService = bidsService;
+            this.commentsService = commentsService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
